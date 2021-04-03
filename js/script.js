832ee5e5816a575dee56055e12421b83bb8a337c
aoshiro <koh.sss0629@gmail.com>
2021-04-03 10:38:53 +0900

    スライドメニューとオーバーレイの表示速度調整 200に設定

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ $(function() {
     $(this).toggleClass("open");
     $("body").toggleClass("open");
     if($(this).hasClass("open")) {
-      overray.fadeIn();
+      overray.fadeIn(200);
       nav.stop().animate({"right": 0}, 200,);
     } else {
-      overray.fadeOut();
+      overray.fadeOut(200);
       nav.stop().animate({"right": - outerWidth}, 200,);
     }
   });
